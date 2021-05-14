e7c70c319ef04ca7e782028cfe495ffc618dcece
YohannHommet <yohann.hommet@outlook.fr>
2021-05-14 18:03:20 +0200

    Modif form

--- src/Form/InscriptionsType.php
+++ src/Form/InscriptionsType.php
@@ -6,6 +6,11 @@ use App\Entity\Inscriptions;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class InscriptionsType extends AbstractType
 {
@@ -14,14 +19,31 @@ class InscriptionsType extends AbstractType
         $builder
             ->add('firstname')
             ->add('lastname')
-            ->add('isOk')
-            ->add('email')
-            ->add('phone')
-            ->add('adress')
-            ->add('cp')
-            ->add('city')
-            ->add('department')
-            ->add('note')
+            ->add('email', EmailType::class, [
+                'label' => "Votre email",
+                'help' => "* l'email restera confidentiel"
+            ])
+            ->add('phone', TelType::class, [
+                'label' => "Téléphone",
+                'help' => "* le numéro restera confidentiel"
+            ])
+            ->add('adress', TextType::class, [
+                'label' => "Votre adresse",
+                'help' => "* l'adresse restera confidentielle"
+            ])
+            ->add('cp', IntegerType::class, [
+                'label' => "Votre Code Postal",
+            ])
+            ->add('city', TextType::class, [
+                'label' => "Votre Ville",
+            ])
+            ->add('department', TextType::class, [
+                'label' => "Votre département",
+            ])
+            ->add('note', TextareaType::class, [
+                'label' => "Remarques",
+                'help' => "Ex: J'apporte à boire, j'aime me faire conduire, je suis célib... :)"
+            ])
         ;
     }
 
